252c041e9e78d2e7a194c2f770e54c3338aaab9c
Daniel Felipe Santana Batista <daniel.sbatista@shopee.com>
2026-06-27 11:43:12 -0300

    feat: Add more endpoints into CRUD router

--- 27 - CRUD/main.go
+++ 27 - CRUD/main.go
@@ -12,6 +12,8 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/users", routers.CreateUser).Methods(http.MethodPost) // Post
+	router.HandleFunc("/users", routers.GetUsers).Methods(http.MethodGet) // Get
+	router.HandleFunc("/users/{id}", routers.GetUserById).Methods(http.MethodGet) // Get
 
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
--- 27 - CRUD/routers/router.go
+++ 27 - CRUD/routers/router.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type UserSchema struct{
@@ -56,4 +59,87 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(fmt.Sprintf("User added successfully, ID: %d", idInserted)))
+}
+
+
+func GetUsers(w http.ResponseWriter, r *http.Request){
+	db, err := database.Connection()
+	if err != nil{
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("An error occurred while connecting on database"))
+		return 
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT nome, email FROM public.usuario")
+	if err != nil{
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("An error occurred while executing query"))
+		return 
+	}
+	defer rows.Close()
+	
+	var users []UserSchema = []UserSchema{}
+	for rows.Next(){
+		var user UserSchema
+		if err := rows.Scan(&user.Nome, &user.Email); err != nil{
+			 w.Write([]byte("An error occured while compiling the rows"))
+			 return
+		}
+		users = append(users, user)
+	}
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(users); err != nil{
+		w.Write([]byte("An error occured while encoding users list"))
+		return 
+	}
+
+}
+
+
+func GetUserById(w http.ResponseWriter, r *http.Request){
+	
+	params := mux.Vars(r)
+	id, err := strconv.ParseUint(params["id"], 10, 32)
+	if err != nil{
+		w.Write([]byte("An error occured while parsing id param"))
+		return 
+	}
+
+	db, err := database.Connection()
+	if err != nil{
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("An error occurred while connecting on database"))
+		return 
+	}
+	defer db.Close()
+
+	rows, err := db.Query(`
+		SELECT nome, email FROM public.usuario
+		WHERE id = $1
+	`, id)
+	if err != nil{
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("An error occurred while executing query"))
+		return 
+	}
+	defer rows.Close()
+	
+	rows.Next()
+
+	var user UserSchema
+	if err := rows.Scan(&user.Nome, &user.Email); err != nil{
+			w.Write([]byte("An error occured while compiling the rows"))
+			return
+	}
+	
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(user); err != nil{
+		w.Write([]byte("An error occured while encoding users list"))
+		return 
+	}
 }
